924d7a5652c1cb717da5d5d712ddaaedda4fae85
skwarek <mateusz.sekara@gmail.com>
2015-03-30 13:42:04 +0200

    Meeting summary

---
+++ etc/draft/Diagram.js
@@ -0,0 +1,19 @@
+var representationManager = function (adaptationSet) {
+
+    var presentRepresentation;
+
+    return {
+        //returns url
+        switchRepresentationToHigher: function () {
+
+        },
+
+        switchRepresentationToLower: function () {
+
+        },
+
+        getNextElement: function () {
+
+        }
+    }
+};
--- src/Utils.js
+++ src/Utils.js
@@ -1,5 +1,6 @@
 var Utils = function () {
     var rangeParam = "&range=";
+    //fixme remove sparams, consider moving to separate class which will be reponsible for all actions connected with choosing representation
     var lastParam = "&sparams";
     return {
         addRangeToBaseUrl: function (baseUrl, start, end) {
@@ -7,4 +8,6 @@ var Utils = function () {
             return splitted[0] + rangeParam + start + "-" + end + lastParam + splitted[1];
         }
     }
+
+
 };
