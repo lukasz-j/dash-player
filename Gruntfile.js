622d1c761d4d7dbd028913d898f7d4913288762c
skwarek <mateusz.sekara@gmail.com>
2015-08-30 00:15:04 +0200

    draft

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,6 +51,15 @@ module.exports = function (grunt) {
             }
         },
 
+        connect: {
+            server: {
+                options: {
+                    port: 9001,
+                    base: '.'
+                }
+            }
+        },
+
         jasmine: {
             test: {
                 src: 'src/**/*.js',
@@ -58,10 +67,14 @@ module.exports = function (grunt) {
                     specs: 'test/spec/**/*Spec.js',
                     helpers: 'test/helper/*Helper.js',
                     template: require('grunt-template-jasmine-istanbul'),
+                    host: 'http://localhost:9001/',
                     templateOptions: {
                         coverage: 'test/coverage/coverage.json',
                         report: 'test/coverage'
-                    }
+                    },
+                    vendor: [
+                        "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"
+                    ]
                 }
             }
         }
@@ -71,7 +84,8 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-react');
 
-    grunt.registerTask('default', ['concat', 'jshint', 'uglify', 'react', 'jasmine']);
+    grunt.registerTask('default', ['concat', /*'jshint',*/ 'uglify', 'react', 'connect', 'jasmine']);
 };
--- test/spec/mpd/MpdParserSpec.js
+++ test/spec/mpd/MpdParserSpec.js
@@ -1,5 +1,42 @@
-describe("A suite", function() {
-    it("contains spec with an expectation", function() {
-        expect(true).toBe(true);
+describe("Parser", function () {
+    var resourcesLocation = 'http://localhost:9001/test/resources/mpd/',
+    //var resourcesLocation = 'http://localhost:63342/dash-msc/test/resources/mpd/',
+        eventBus,
+        parser;
+
+    beforeEach(function () {
+        eventBus = Dash.event.EventBus();
+        parser = Dash.mpd.Parser(eventBus);
     });
+
+    describe('parsing jaguar_youtube.mpd file', function () {
+        var mpdFileContent,
+            mpdFileURL = resourcesLocation + 'jaguar_youtube.mpd';
+
+        beforeEach(function (done) {
+            $.ajax({
+                url: mpdFileURL,
+                contentType: 'text/xml'
+            }).done(function (data) {
+                mpdFileContent = data;
+                done();
+            });
+        });
+
+        it('should return valid model', function (done) {
+            var mpdModel = parser.generateModel(mpdFileContent, mpdFileURL, false);
+            /*   expect(mpdModel).toBeDefined();
+
+             var period = mpdModel.getPeriod();*/
+
+            //expect(mpdFileContent).toBe(true);
+
+
+//.log(mpdFileContent);
+
+            expect(typeof mpdFileContent === 'string').toBe(true);
+            done();
+        });
+    });
+
 });
