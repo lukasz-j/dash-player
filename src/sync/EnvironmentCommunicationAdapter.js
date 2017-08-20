10ffe5000c991cc9c00fe9ca4178662ab1388ee5
Łukasz Jeleń <lukasz.jelen@tape.international>
2017-08-20 17:31:21 +0200

    Adapters for adaptation profiles in ext. comm. manager

--- src/sync/EnvironmentCommunicationAdapter.js
+++ src/sync/EnvironmentCommunicationAdapter.js
@@ -2,6 +2,12 @@ Dash.sync.EnvironmentCommunicationAdapter = function() {
     return {
         setDownloaderProxy: function(callback) {
             Dash.utils.setAsyncDownloaderProxy(callback);
+        },
+        injectAdaptationProfiles: function(data) {
+            dashPlayer.adaptationManager.importProfiles(data);
+        },
+        provideAdaptationProfilesForPersistence: function() {
+            return dashPlayer.adaptationManager.exportProfiles();
         }
     };
 };
